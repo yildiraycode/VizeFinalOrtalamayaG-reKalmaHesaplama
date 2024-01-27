884cc45778e4924e8a1e46a83c6b6328409bdffe
Yıldıray ALADAĞ <128586234+yildiraycode@users.noreply.github.com>
2024-01-27 21:48:15 +0300

    Add project files.

---
+++ VizeFinalOrtalamayaGöreKalmaHesaplama/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿
+namespace VizeFinalOrtalamayaGöreKalmaHesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.txtVize = new System.Windows.Forms.TextBox();
+            this.txtFinal = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.listBoxAdSoyad = new System.Windows.Forms.ListBox();
+            this.listBoxVize = new System.Windows.Forms.ListBox();
+            this.listBoxFinal = new System.Windows.Forms.ListBox();
+            this.listBoxOrtalama = new System.Windows.Forms.ListBox();
+            this.listBoxDurum = new System.Windows.Forms.ListBox();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(39, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adı ve Soyadı:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(81, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Vize notu:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(75, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(119, 27);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Final notu:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(111, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "(%40 etkili)*";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(111, 213);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "(%60 etkili)*";
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtAdSoyad.Location = new System.Drawing.Point(210, 62);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(154, 28);
+            this.txtAdSoyad.TabIndex = 5;
+            // 
+            // txtVize
+            // 
+            this.txtVize.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtVize.Location = new System.Drawing.Point(210, 113);
+            this.txtVize.Name = "txtVize";
+            this.txtVize.Size = new System.Drawing.Size(154, 28);
+            this.txtVize.TabIndex = 6;
+            // 
+            // txtFinal
+            // 
+            this.txtFinal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtFinal.Location = new System.Drawing.Point(210, 180);
+            this.txtFinal.Name = "txtFinal";
+            this.txtFinal.Size = new System.Drawing.Size(154, 28);
+            this.txtFinal.TabIndex = 7;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.BackColor = System.Drawing.Color.DarkGray;
+            this.btnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.Location = new System.Drawing.Point(427, 162);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(178, 45);
+            this.btnHesapla.TabIndex = 8;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = false;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // listBoxAdSoyad
+            // 
+            this.listBoxAdSoyad.FormattingEnabled = true;
+            this.listBoxAdSoyad.ItemHeight = 16;
+            this.listBoxAdSoyad.Location = new System.Drawing.Point(23, 307);
+            this.listBoxAdSoyad.Name = "listBoxAdSoyad";
+            this.listBoxAdSoyad.Size = new System.Drawing.Size(156, 164);
+            this.listBoxAdSoyad.TabIndex = 9;
+            // 
+            // listBoxVize
+            // 
+            this.listBoxVize.FormattingEnabled = true;
+            this.listBoxVize.ItemHeight = 16;
+            this.listBoxVize.Location = new System.Drawing.Point(238, 307);
+            this.listBoxVize.Name = "listBoxVize";
+            this.listBoxVize.Size = new System.Drawing.Size(156, 164);
+            this.listBoxVize.TabIndex = 10;
+            // 
+            // listBoxFinal
+            // 
+            this.listBoxFinal.FormattingEnabled = true;
+            this.listBoxFinal.ItemHeight = 16;
+            this.listBoxFinal.Location = new System.Drawing.Point(436, 307);
+            this.listBoxFinal.Name = "listBoxFinal";
+            this.listBoxFinal.Size = new System.Drawing.Size(156, 164);
+            this.listBoxFinal.TabIndex = 11;
+            // 
+            // listBoxOrtalama
+            // 
+            this.listBoxOrtalama.FormattingEnabled = true;
+            this.listBoxOrtalama.ItemHeight = 16;
+            this.listBoxOrtalama.Location = new System.Drawing.Point(642, 307);
+            this.listBoxOrtalama.Name = "listBoxOrtalama";
+            this.listBoxOrtalama.Size = new System.Drawing.Size(156, 164);
+            this.listBoxOrtalama.TabIndex = 12;
+            // 
+            // listBoxDurum
+            // 
+            this.listBoxDurum.FormattingEnabled = true;
+            this.listBoxDurum.ItemHeight = 16;
+            this.listBoxDurum.Location = new System.Drawing.Point(849, 307);
+            this.listBoxDurum.Name = "listBoxDurum";
+            this.listBoxDurum.Size = new System.Drawing.Size(156, 164);
+            this.listBoxDurum.TabIndex = 13;
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.BackColor = System.Drawing.Color.DarkGray;
+            this.btnTemizle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTemizle.Location = new System.Drawing.Point(632, 163);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(178, 45);
+            this.btnTemizle.TabIndex = 14;
+            this.btnTemizle.Text = "Temizle";
+            this.btnTemizle.UseVisualStyleBackColor = false;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(50, 286);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 18);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Adı ve Soyadı";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(267, 286);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(78, 18);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Vize notu";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(469, 286);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(82, 18);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Final notu";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(676, 286);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(77, 18);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Ortalama";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.Location = new System.Drawing.Point(883, 286);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(58, 18);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "Durum";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(1059, 504);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnTemizle);
+            this.Controls.Add(this.listBoxDurum);
+            this.Controls.Add(this.listBoxOrtalama);
+            this.Controls.Add(this.listBoxFinal);
+            this.Controls.Add(this.listBoxVize);
+            this.Controls.Add(this.listBoxAdSoyad);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.txtFinal);
+            this.Controls.Add(this.txtVize);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "VizeFinalOrtalamaHesaplama";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.TextBox txtVize;
+        private System.Windows.Forms.TextBox txtFinal;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.ListBox listBoxAdSoyad;
+        private System.Windows.Forms.ListBox listBoxVize;
+        private System.Windows.Forms.ListBox listBoxFinal;
+        private System.Windows.Forms.ListBox listBoxOrtalama;
+        private System.Windows.Forms.ListBox listBoxDurum;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ VizeFinalOrtalamayaGöreKalmaHesaplama/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VizeFinalOrtalamayaGöreKalmaHesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            listBoxAdSoyad.Items.Add(txtAdSoyad.Text);
+            int vize, final;
+            double ortalama;
+
+            vize = Convert.ToInt32(txtVize.Text);
+            final = Convert.ToInt32(txtFinal.Text);
+
+            ortalama = vize * 0.4 + final * 0.6;
+            listBoxVize.Items.Add(txtVize.Text);
+            listBoxFinal.Items.Add(txtFinal.Text);
+            listBoxOrtalama.Items.Add(ortalama);
+            if (ortalama >= 50)
+            {
+                listBoxDurum.Items.Add("GEÇTİ");
+            }
+            else
+            {
+                listBoxDurum.Items.Add("KALDI");
+            }
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            txtAdSoyad.Clear();
+            txtVize.Clear();
+            txtFinal.Clear();
+
+        }
+    }
+}
